054e1fc181e9a51e5a2fe12fafbe08d873ca734f
Alan Felipe Robles Diaz <roblesdiazalan@gmail.com>
2026-03-23 23:45:14 -0400

    fix: resolve merge conflicts and cleanup code

--- packages/backend/src/contexts/academic/academic.module.ts
+++ packages/backend/src/contexts/academic/academic.module.ts
@@ -2,19 +2,10 @@ import { Module } from '@nestjs/common';
 import { StudentModule } from './student/student.module';
 import { ScheduleModule } from './schedule/schedule.module';
 import { CourseModule } from './course/course.module';
-<<<<<<< HEAD
-import { DepartmentModule } from './department/department.module'; // <-- ESTO
+import { DepartmentModule } from './department/department.module';
 
 @Module({
-  imports: [StudentModule, ScheduleModule, CourseModule, DepartmentModule], // <-- Y ESTO
+  imports: [StudentModule, ScheduleModule, CourseModule, DepartmentModule],
   exports: [StudentModule, ScheduleModule, CourseModule, DepartmentModule],
 })
 export class AcademicModule {}
-=======
-
-@Module({
-  imports: [StudentModule, ScheduleModule, CourseModule],
-  exports: [StudentModule, ScheduleModule, CourseModule],
-})
-export class AcademicModule {}
->>>>>>> 00efccea6476f5df81d3370e2033824234b5dd82
